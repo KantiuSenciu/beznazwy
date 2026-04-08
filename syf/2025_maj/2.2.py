daaa78033afb14f8d7a133aaa9793df9fc1cec49
KantiuSenciu <ala.gawel.2007@gmail.com>
2026-04-08 21:29:35 +0200

    Update 2.2.py
    dokonczenie tego zadanka

--- syf/2025_maj/2.2.py
+++ syf/2025_maj/2.2.py
@@ -1,17 +1,32 @@
-## do skonczenia
+p = open("symbole.txt", 'r')
+plik = [linia.strip() for linia in p]
+znaki = len(plik[0]) # liczba znaków w wierszu
 
+p.close()
 
-plik = open("symbole.txt")
-plik = plik.read()
-znaki = 12 # liczba znaków w wierszu 
+kwadraty = []
 
-
-for i in range(0, len(plik) - 2):
-    for j in range(0, znaki-2): # te 2 to odejmujemy bo szukamy kwadratu 3x3 wiec zeby nie wywalilo tam nieistniejacych indeksow
+for i in range(len(plik) - 2):
+    for j in range(znaki-2): # te -2 to odejmujemy bo szukamy kwadratu 3x3 wiec zeby nie wyjebalo tam nieistniejacych indeksow
         w1 = plik[i]
         w2 = plik[i+1]
         w3 = plik[i+2]
 
-        #z1 = w1[j] + w1[j+1] + w1[j+2]
+        z1 = w1[j] + w1[j+1] + w1[j+2]
+        z2 = w2[j] + w2[j+1] + w2[j+2]
+        z3 = w3[j] + w3[j+1] + w3[j+2]
 
-        #print(z1)
+        linia = (z1[0] == z1[1] == z1[2]) # czy w lini sa wszystkie znaki jednakowe
+        kwadrat = (z1 == z2 == z3) # czy w kwadracie sa wszystkie linie jednakowe
+
+        if linia and kwadrat:
+
+            wspolrzedne = i+2, j+2
+            kwadraty.append(wspolrzedne)
+
+
+
+print('liczba kwadratow: ',len(kwadraty))
+print('wspolrzedne srodkow:')
+for wiersz,kolumna in kwadraty:
+    print(wiersz,kolumna)
